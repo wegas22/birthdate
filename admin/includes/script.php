f825c1b3dcf0eb66928444abca0bad50378a8fb0
Wegas <wegas22@yandex.ru>
2019-08-07 11:38:13 +0400

    commit

--- admin/includes/script.php
+++
@@ -1,6 +0,0 @@
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-    <script type="text/javascript" src="/admin/js/jquery-1.10.2.min.js"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-    <script type="text/javascript" src="/admin/bootstrap/js/bootstrap.min.js"></script>
--- admin/includes/style.php
+++
@@ -1,4 +0,0 @@
-    <link rel="stylesheet" type="text/css" href="/admin/bootstrap/css/bootstrap.min.css" />
-    <link rel="stylesheet" type="text/css" href="/admin/font-awesome/css/font-awesome.min.css" />
-    <link rel="stylesheet" type="text/css" href="/admin/css/local.css" />
-    <link rel="stylesheet" type="text/css" href="/admin/css/styles.css"  />
